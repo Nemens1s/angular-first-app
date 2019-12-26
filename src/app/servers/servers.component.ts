6c5b07222cc87cf11c879fb9b1c4119752b557ee
Ilja <ilniko@taltech.ee>
2019-12-26 23:24:26 +0200

    passing data from form. enabled FormModule

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreatinStatus = "No server was created";
+  serverName = '';
   constructor() {
     setTimeout(()=> {
       this.allowNewServer = true;
@@ -21,4 +22,8 @@ export class ServersComponent implements OnInit {
     this.serverCreatinStatus = "Server was created";
   }
 
+  onUpdatedServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value
+  }
 }
